1d174ce4b440582d0c3f587dd4ef2cf22a6d4b1f
Danh Nguyen <nguyendanhtravel@gmail.com>
2026-03-09 07:51:29 +0700

    feat: only show on minibar

--- Sources/EyeBreak/App/EyeBreakApp.swift
+++ Sources/EyeBreak/App/EyeBreakApp.swift
@@ -7,7 +7,7 @@ enum EyeBreakLauncher {
     static func main() {
         let app = NSApplication.shared
         app.delegate = appDelegate
-        app.setActivationPolicy(.regular)
+        app.setActivationPolicy(.accessory)
         app.run()
     }
 }
